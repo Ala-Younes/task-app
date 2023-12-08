077b0989828d335ae4c52e27f8b889ddfa3e2048
ALA YOUNES <ala.younes@eviden.com>
2023-12-08 17:03:14 +0100

    main - Update - react hot toast

--- README.md
+++ README.md
@@ -2,5 +2,10 @@
 
 ## Many Theme Mode toggling
 
-![light Mode](image-3.png)
-![dark Mode](image-2.png)
+### Light
+
+![light Mode](image-5.png)
+
+### Dark
+
+![dark Mode](image-4.png)
--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Task from "./models/Task";
 import EditTask from "./components/EditTask";
 import useTheme from "./hooks/useTheme";
 import useTask from "./hooks/useTask";
+import toast, { Toaster } from "react-hot-toast";
 
 function App() {
   const { activeTheme, handleThemeChange } = useTheme("gray");
@@ -22,21 +23,33 @@ function App() {
   const handleTaskDelete = (id: number) => {
     deleteTask(id);
     resetForm();
+    toast.success("Success Deleting Task", {
+      position: "top-right",
+    });
   };
 
   const handleTaskAdd = (taskName: string) => {
     addTask(taskName);
     resetForm();
+    toast.success("Success Adding Task", {
+      position: "top-right",
+    });
   };
 
   const handleTaskEdit = (taskName: string) => {
     editTask(taskToEdit?.id, taskName);
     resetForm();
+    toast.success("Success Editing Task", {
+      position: "top-right",
+    });
   };
 
   const handleTasksClear = () => {
     clearTasks();
     resetForm();
+    toast.success("Success Clearing Tasks", {
+      position: "top-right",
+    });
   };
 
   const handleEditClick = (editedTask: Task) => {
@@ -47,6 +60,7 @@ function App() {
   return (
     // ! Work around to apply bg-dark on all the h-screen
     <div className={`${activeTheme} bg-black`}>
+      <Toaster />
       <div className={`h-screen mx-auto max-w-6xl py-16 dark:bg-black`}>
         <Header onThemeChange={handleThemeChange} />
         {editMode === "add" ? (
